669e81f5aaec7b8ef134cba736ebe44af877ae1a
MTOR <129797537+MohcineTor@users.noreply.github.com>
2023-05-24 16:10:27 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,21 @@
 def buildJar() {
-    echo "building the application..."
+    echo "Building the Jar of Application....."
     sh 'mvn package'
-} 
-
+}
+def testApp() {
+    echo "Testing the Application ..."
+    sh 'mvn test'
+}
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    echo "Building the Image docker for our Application and push this in dockerHub repo ..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh 'docker build -t dockermohcine/my-repo/test-app:jma-3.0 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push dockermohcine/my-repo/test-app:jma-3.0'
     }
-} 
-
+}
 def deployApp() {
-    echo 'deploying the application...'
-} 
-
-return this
+    echo "Deploying application...."
+    sh 'docker run -d -p 8080:8080 dockermohcine/my-repo/test-app:jma-3.0'
+}
+return this 
