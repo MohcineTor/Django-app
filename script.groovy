3a0e5ce25f8079f1e104392c10a3ab95a1cb03b0
MTOR <129797537+MohcineTor@users.noreply.github.com>
2023-07-23 21:32:04 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,14 @@
 
+def increment_version() {
+    echo 'incrementing version app version ....'
+    sh 'mvn build-helper:parse-version versions:set \
+            -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+            versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
 def buildJar() {
     echo "Building the Jar of Application....."
     sh 'mvn clean package'
